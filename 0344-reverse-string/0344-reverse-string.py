a290a9d2198b7e77dcfc1399529848f50816f965
Mihret4204 <mihrettena1@gmail.com>
2026-02-16 13:30:54 +0300

    Time: 0 ms (100%), Space: 23.5 MB (54.95%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,8 +3,6 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        n=len(s)
-        for i in range (n//2):
-            s[i],s[n-i-1]=s[n-i-1],s[i]
+        s=s.reverse()
         print(s)
         
