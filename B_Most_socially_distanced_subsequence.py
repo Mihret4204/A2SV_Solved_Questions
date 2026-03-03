b7d907ac8780c1649b18d48471d7c4f405303397
Mihret <mihretina@gmail.com>
2026-03-03 18:42:11 +0300

    codeForce problem solution added

---
+++ B_Most_socially_distanced_subsequence.py
@@ -0,0 +1,21 @@
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    ans=0
+    res=[]
+
+    #r=a[1]
+    res.append(a[0])
+    l=a[0]
+    prev=a[1]-l
+    for i in range(2,n):
+        curr=a[i]-a[i-1]
+        if curr*prev<0:
+            res.append(a[i-1])
+        prev=curr
+    
+    res.append(a[-1])
+    print(len(res))
+    print(*res)
+
