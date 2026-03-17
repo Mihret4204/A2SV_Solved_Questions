9d93a88bc2228afb64d151edd445bd97ddc3c88c
Mihret4204 <mihrettena1@gmail.com>
2026-03-17 18:27:46 +0300

    Add solution for Array Splitting

---
+++ codeforces/1197C-Array-Splitting/1197C-Array-Splitting.py
@@ -0,0 +1,12 @@
+n,m=map(int,input().split())
+arr=list(map(int,input().split()))
+a=[]
+if n==m:
+    print(0)
+elif m==1:
+    print(max(arr)-min(arr))
+else:
+    for i in range(n-1):
+        a.append(arr[i+1]-arr[i])
+    a.sort()
+    print(sum(a[:n-m]))
