b3b5fab3a7005162ea5a93bc1675c064d855e7db
Mihret4204 <mihrettena1@gmail.com>
2026-03-24 22:56:18 +0300

    Time: 0 ms (100%), Space: 19.5 MB (31.77%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        miss = 1
+        ans = 0
+        i = 0
+
+        while miss <= n:
+            if i < len(nums) and nums[i] <= miss:
+                miss += nums[i]
+                i += 1
+            else:
+                miss += miss
+                ans += 1
+
+        return ans
+
+        
