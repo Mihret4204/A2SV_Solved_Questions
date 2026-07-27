5bac72ff30ca01efd08e59d39d3683e171dbf479
Mihret Tena <mihrettena1@gmail.com>
2026-07-27 23:53:22 +0300

    Time: 7 ms (66.58%), Space: 23.6 MB (27.71%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,10 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
+        my_dic = Counter(nums)
+        ans = []
         i=len(nums)//3
-        my_dic={}
-        ans=[]
-        for num in nums:
-            my_dic[num]=my_dic.get(num,0)+1
         for idx,val in my_dic.items():
             if val>i:
                 ans.append(idx)            
