cf48a86c97ef903ecc7616a7f5594ae19c58c51c
Mihret Tena <mihrettena1@gmail.com>
2026-07-31 22:16:05 +0300

    Time: 1534 ms (5.01%), Space: 29.3 MB (6.06%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.py
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.py
@@ -1,15 +1,18 @@
 class Solution:
     def maxActiveSectionsAfterTrade(self, s: str) -> int:
-        ones = s.count("1")
-        t = "1" + s + "1"
+        total = Counter(s)
+        print(total)
+        s='1'+s+'1'
         arr = []
-        for ch in t:
-            if not arr or arr[-1][0] != ch:
-                arr.append([ch, 1])
+        curr='-1'
+        for i in range(1,len(s)-1):
+            if arr and  s[i]==arr[-1][0]:
+                arr[-1][1]+=1
             else:
-                arr[-1][1] += 1
-        best = 0
-        for i in range(1, len(arr) - 1):
-            if (arr[i][0] == "1" and arr[i - 1][0] == "0" and arr[i + 1][0] == "0"):
-                best = max(best, arr[i - 1][1] + arr[i + 1][1])
-        return ones + best
+                arr.append([s[i],1])
+        ans = 0
+        mx = 0
+        for i in range(1,len(arr)-1):
+            if arr[i][0]=='1' and arr[i-1][0]=='0' and arr[i+1][0]=='0':
+                mx = max((arr[i-1][1]+arr[i+1][1]),mx)
+        return total['1']+mx
