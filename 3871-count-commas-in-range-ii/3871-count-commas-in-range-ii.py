40fa31ad46253f1809117c3d7321c4b0daafd642
Mihret Tena <mihrettena1@gmail.com>
2026-09-09 17:25:35 +0300

    Time: 0 ms (100%), Space: 19.1 MB (98.64%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countCommas(self, n: int) -> int:
+        ans=0
+        x = 1000
+    
+        
+        while n>=x: 
+            ans+=n-x+1            
+            x*=1000
+
+        return ans
