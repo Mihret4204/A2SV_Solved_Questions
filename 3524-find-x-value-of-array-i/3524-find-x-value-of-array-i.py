d32041c964e02a79c936ecb227446d1d1d5c165c
Mihret Tena <mihrettena1@gmail.com>
2026-09-21 23:27:51 +0300

    Time: 343 ms (79.45%), Space: 34.4 MB (24.66%) - LeetHub

---
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.py
@@ -0,0 +1,17 @@
+class Solution:
+    def resultArray(self, nums: List[int], k: int) -> List[int]:
+        ans = freq = [0] * k
+
+        for n in nums:
+            n %= k
+            cur = [0] * k
+            cur[n] = 1
+
+            for x, y in enumerate(freq):
+                cur[x * n % k] += y
+
+            freq = cur
+            for x, y in enumerate(freq):
+                ans[x] += y
+
+        return ans
